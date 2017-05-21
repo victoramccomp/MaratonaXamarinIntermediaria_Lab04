4aa2faa22823b5a6f5c0a6e8531eacce4c8b8868
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-05-21 11:49:24 -0300

    Fisrtt commit lab04

---
+++ SocialLoginLab/SocialLoginLab.Android/AuthenticateDroid.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.WindowsAzure.MobileServices;
+using SocialLoginLab.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AuthenticateDroid))]
+namespace SocialLoginLab.Droid
+{
+    public class AuthenticateDroid : IAuthenticate
+    {
+        public async Task<MobileServiceUser> Authenticate(MobileServiceClient client,
+            MobileServiceAuthenticationProvider provider)
+        {
+            try
+            {
+                return await client.LoginAsync(Xamarin.Forms.Forms.Context, provider);
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw;
+            }
+        }
+    }
+}
---
+++ SocialLoginLab/SocialLoginLab/IAuthenticate.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+
+namespace SocialLoginLab
+{
+    public interface IAuthenticate
+    {
+        Task<MobileServiceUser> Authenticate(MobileServiceClient client,
+            MobileServiceAuthenticationProvider provider);
+    }
+}
