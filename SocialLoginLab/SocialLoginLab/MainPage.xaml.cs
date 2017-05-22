8fce5cfa39073225487f864eb3c0a26cf2933267
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-05-21 21:39:24 -0300

    Layout and click event Login Facebook

--- SocialLoginLab/SocialLoginLab/MainPage.xaml.cs
+++ SocialLoginLab/SocialLoginLab/MainPage.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace SocialLoginLab
 {
     public partial class MainPage : ContentPage
     {
+        readonly AzureService azureService = new AzureService();
+
         public MainPage()
         {
             InitializeComponent();
+
+            LoginButton.Clicked += async (sender, args) =>
+            {
+                var user = await azureService.LoginAsync();
+                InfoLabel.Text = (user != null) ? $"Bem vindo: {user.UserId}" : "Falha no Login, tente novamente!";
+            };
         }
     }
 }
--- SocialLoginLab/SocialLoginLab/obj/Debug/SocialLoginLab.MainPage.xaml.g.cs
+++ SocialLoginLab/SocialLoginLab/obj/Debug/SocialLoginLab.MainPage.xaml.g.cs
@@ -16,8 +16,16 @@ namespace SocialLoginLab {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button LoginButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label InfoLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            LoginButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "LoginButton");
+            InfoLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "InfoLabel");
         }
     }
 }
